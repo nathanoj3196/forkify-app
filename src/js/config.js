4cd31791d9446650df9d23c8d588eadfee91266b
nathanoj3196 <nathanoj31carlo@gmail.com>
2025-07-04 10:17:10 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5cd1c2a0-3197-4484-8f2e-a846b82212aa';
+export const MODAL_CLOSE_SEC = 2.5;
